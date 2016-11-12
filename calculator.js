edc7d938ccb008a3bd3866349403e91d2b499b40
Randy Salazar <randysalazar@Randys-MacBook-Pro.local>
2016-11-11 14:48:59 -1000

    completed everything but the load function.

--- calculator.js
+++ calculator.js
@@ -6,30 +6,49 @@
  * @return {object} `calculator` object that can be used
  */
 
+function calculatorModule(){
+  
+  var calculator = { };
+  var _memory = 0;
+  var _total = 0;
+
+
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
-
+  calculator.load= function(x){
+    _total = x;
+    return _total;
+  };
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+   calculator.getTotal = function (x){
+    return _total;
+   };
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+   calculator.add = function(x){
+    _total = _total + x;
+  };
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+
+   calculator.subtract = function (x){
+    _total = _total - x;
+   };
 
 
   /**
@@ -37,29 +56,56 @@
    * @param  { Number } x
    */
 
+   calculator.multiply = function (x){
+    _total = _total * x;
+   };
+
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   calculator.divide = function (x){
+    _total = _total / x;
+   };
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   calculator.recallMemory = function(x){
+    return _memory;
+   };
 
   /**
    * Stores the value of `total` to `memory`
    */
 
+   calculator.saveMemory = function (x){
+    _memory = _total;
+   };
 
   /**
    * Clear the value stored at `memory`
    */
 
+   calculator.clearMemory = function (x){
+    _memory = 0;
+   };
+
   /**
    * Validation
    */
 
+  calculator.load = function(x){
+    if( x === "number"){
+      return throw error;
+    }
+
+  }
+
+
+   return calculator;
+}
